0b88cfa580b5f84f4a75e5786717c734e8f2ff85
alru28 <alvaroruiz@opendeusto.es>
2024-03-25 19:32:50 +0100

    Basic Project Structure (Client/Server) Initialization

---
+++ bike-lending-app/src/main/java/com/deusto/app/client/BikeClient.java
@@ -0,0 +1,31 @@
+package com.deusto.app.client;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class BikeClient implements ActionListener, Runnable {
+	
+
+	public BikeClient(String hostname, String port) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		String hostname = args[0];
+		String port = args[1];
+
+		new BikeClient(hostname, port);
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+}
