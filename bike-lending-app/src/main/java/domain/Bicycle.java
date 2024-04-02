3bb596f7571ac5cedca82c5436c8ee05401bcc88
aitortxuh <a.hernandez@opendeusto.es>
2024-04-02 13:05:01 +0200

    Station entity

--- bike-lending-app/src/main/java/domain/Bicycle.java
+++ bike-lending-app/src/main/java/domain/Bicycle.java
@@ -1,4 +1,4 @@
-package com.deusto.app.server;
+package domain;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
---
+++ bike-lending-app/src/main/java/domain/Station.java
@@ -0,0 +1,47 @@
+package domain;
+
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.PrimaryKey;
+
+@PersistenceCapable(detachable="true")
+public class Station {
+    @PrimaryKey
+    private int ID;
+    private String location;
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String toString() {
+        StringBuilder result = new StringBuilder();
+
+        result.append("Station ID: ");
+        result.append(this.ID);
+        result.append(" / Location: ");
+        result.append(this.location);
+
+        return result.toString();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this.getClass().getName().equals(obj.getClass().getName())) {
+            return this.ID == ((Station)obj).ID;
+        }
+
+        return false;
+    }
+}
