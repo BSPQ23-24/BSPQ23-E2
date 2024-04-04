8879fdbd9e4ce3d2a1d5f054daf6c424c697b20e
alru28 <alvaroruiz@opendeusto.es>
2024-04-04 17:49:24 +0200

    Intento 1 de Servicio de Registro

--- bike-lending-app/src/main/java/com/deusto/app/server/data/domain/User.java
+++ bike-lending-app/src/main/java/com/deusto/app/server/data/domain/User.java
@@ -7,94 +7,113 @@ import java.util.Locale;
 import javax.jdo.annotations.PersistenceCapable;
 import javax.jdo.annotations.PrimaryKey;
 
-@PersistenceCapable(detachable="true")
+@PersistenceCapable(detachable = "true")
 public class User {
-    @PrimaryKey
-    private String DNI;
-    private String name;
-    private String surname;
-    private Date dateOfBirth;
-    private String phone;
-    private String mail;
+	@PrimaryKey
+	private String dni;
+	private String name;
+	private String password;
+	private String surname;
+	private Date dateOfBirth;
+	private String phone;
+	private String mail;
+	
+	public User(String dni, String name, String password, String surname, Date dateOfBirth, String phone, String mail) {
+		super();
+		this.dni = dni;
+		this.name = name;
+		this.password = password;
+		this.surname = surname;
+		this.dateOfBirth = dateOfBirth;
+		this.phone = phone;
+		this.mail = mail;
+	}
 
+	public String getDni() {
+		return dni;
+	}
 
-    public String getDNI() {
-        return DNI;
-    }
+	public void setDNI(String dni) {
+		this.dni = dni;
+	}
 
-    public void setDNI(String DNI) {
-        this.DNI = DNI;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public String getSurname() {
+		return surname;
+	}
 
-    public String getSurname() {
-        return surname;
-    }
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
 
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
+	public Date getDateOfBirth() {
+		return dateOfBirth;
+	}
 
-    public Date getDateOfBirth() {
-        return dateOfBirth;
-    }
+	public void setDateOfBirth(Date dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
 
-    public void setDateOfBirth(Date dateOfBirth) {
-        this.dateOfBirth = dateOfBirth;
-    }
+	public String getPhone() {
+		return phone;
+	}
 
-    public String getPhone() {
-        return phone;
-    }
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
 
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
+	public String getMail() {
+		return mail;
+	}
 
-    public String getMail() {
-        return mail;
-    }
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
 
-    public void setMail(String mail) {
-        this.mail = mail;
-    }
+	public String getPassword() {
+		return password;
+	}
 
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
-    public String toString() {
-        SimpleDateFormat dateFormatter = new SimpleDateFormat("dd-MMM-yyyy", Locale.getDefault());
+	public String toString() {
+		SimpleDateFormat dateFormatter = new SimpleDateFormat("dd-MMM-yyyy", Locale.getDefault());
 
-        StringBuilder result = new StringBuilder();
+		StringBuilder result = new StringBuilder();
 
-        result.append("DNI: ");
-        result.append(this.DNI);
-        result.append(" / Name: ");
-        result.append(this.name);
-        result.append(" / Surname: ");
-        result.append(this.surname);
-        result.append(" / Date of Birth: ");
-        result.append(dateFormatter.format(this.dateOfBirth));
-        result.append(" / Phone: ");
-        result.append(this.phone);
-        result.append(" / Email: ");
-        result.append(this.mail);
+		result.append("DNI: ");
+		result.append(this.dni);
+		result.append("Password: ");
+		result.append(this.password);
+		result.append(" / Name: ");
+		result.append(this.name);
+		result.append(" / Surname: ");
+		result.append(this.surname);
+		result.append(" / Date of Birth: ");
+		result.append(dateFormatter.format(this.dateOfBirth));
+		result.append(" / Phone: ");
+		result.append(this.phone);
+		result.append(" / Email: ");
+		result.append(this.mail);
 
-        return result.toString();
-    }
+		return result.toString();
+	}
 
+	@Override
+	public boolean equals(Object obj) {
+		if (this.getClass().getName().equals(obj.getClass().getName())) {
+			return this.dni.equals(((User) obj).dni);
+		}
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this.getClass().getName().equals(obj.getClass().getName())) {
-            return this.DNI.equals(((User)obj).DNI);
-        }
-
-        return false;
-    }
+		return false;
+	}
 }
--- bike-lending-app/src/main/java/com/deusto/app/server/services/UserService.java
+++ bike-lending-app/src/main/java/com/deusto/app/server/services/UserService.java
@@ -1,23 +1,73 @@
 package com.deusto.app.server.services;
 
+import javax.jdo.JDOHelper;
 import javax.jdo.PersistenceManager;
+import javax.jdo.PersistenceManagerFactory;
+import javax.jdo.Transaction;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import jakarta.ws.rs.POST;
+
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
 import com.deusto.app.server.data.domain.*;
+import com.deusto.app.server.pojo.UserData;
 
+
+@Path("/user")
+@Produces(MediaType.APPLICATION_JSON)
 public class UserService {
+	
+	protected static final Logger logger = LogManager.getLogger();
+	
+	private PersistenceManager pm=null;
+	private Transaction tx=null;
+	
+	public UserService() {
+		PersistenceManagerFactory pmf = JDOHelper.getPersistenceManagerFactory("datanucleus.properties");
+		this.pm = pmf.getPersistenceManager();
+		this.tx = pm.currentTransaction();
+	}
 
-    public void registerUser(String DNI, String name, String surname, Date dateOfBirth, String phone, String mail) {
-        PersistenceManager pm = PMF.get().getPersistenceManager();
-        try {
-            User user = new User();
-            user.setDNI(DNI);
-            user.setName(name);
-            user.setSurname(surname);
-            user.setDateOfBirth(dateOfBirth);
-            user.setPhone(phone);
-            user.setMail(mail);
-            pm.makePersistent(user);
-        } finally {
-            pm.close();
+	@POST
+	@Path("/register")
+    public Response registerUser(UserData userData) {
+        try
+        {	
+            tx.begin();
+            logger.info("Checking whether the user already exits or not: '{}'", userData.getDni());
+			User user = null;
+			try {
+				user = pm.getObjectById(User.class, userData.getDni());
+			} catch (javax.jdo.JDOObjectNotFoundException jonfe) {
+				logger.info("Exception launched: {}", jonfe.getMessage());
+			}
+			logger.info("User: {}", user);
+			if (user != null) {
+				logger.info("Setting password user: {}", user);
+				user.setPassword(userData.getPassword());
+				logger.info("Password set user: {}", user);
+			} else {
+				logger.info("Creating user: {}", user);
+				user = new User(userData.getDni(), userData.getName(), userData.getPassword(), userData.getSurname(), userData.getDateOfBirth(), userData.getPhone(), userData.getMail());
+				pm.makePersistent(user);					 
+				logger.info("User created: {}", user);
+			}
+			tx.commit();
+			return Response.ok().build();
         }
+        finally
+        {
+            if (tx.isActive())
+            {
+                tx.rollback();
+            }
+      
+		}
     }
 }
