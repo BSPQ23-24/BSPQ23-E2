9ba3e01fc85bbc599dc9e0e27f7288c054dff591
Dominik Bobiński <dominik.bobinski@interia.pl>
2024-04-12 21:20:55 +0200

    bike-lending-app/sql/create-bikes.sql: Change "bikesDB" to "bikesdb".
    When running
        mvn datanucleus:schema-create
    An error is returned
        An error was encountered creating a PersistenceManagerFactory : Access denied for user
        'spq'@'localhost' to database 'bikesdb' - please consult the log for more information.
    
    As can be seen in the error message above there is attempted access to the
    "bikesdb" database which does not exist as the name specified in create-bikes.sql
    is "bikesDB". This is not an issue on Windows systems where paths
    are case-insensitive. On Unix-like systems the paths are case-sensitive
    leading to the error above.
    
    To fix this issue all mentions of "bikesDB" are replaced with "bikedb".

--- bike-lending-app/sql/create-bikes.sql
+++ bike-lending-app/sql/create-bikes.sql
@@ -1,11 +1,11 @@
 /* DELETE 'bikesDB' database*/
-DROP SCHEMA IF EXISTS bikesDB;
+DROP SCHEMA IF EXISTS bikesdb;
 /* DELETE USER 'spq' AT LOCAL SERVER*/
 DROP USER IF EXISTS 'spq'@'localhost';
 
 /* CREATE 'messagesDB' DATABASE */
-CREATE SCHEMA bikesDB;
+CREATE SCHEMA bikesdb;
 /* CREATE THE USER 'spq' AT LOCAL SERVER WITH PASSWORD 'spq' */
 CREATE USER IF NOT EXISTS 'spq'@'localhost' IDENTIFIED BY 'spq';
 
-GRANT ALL ON bikesDB.* TO 'spq'@'localhost';
+GRANT ALL ON bikesdb.* TO 'spq'@'localhost';
