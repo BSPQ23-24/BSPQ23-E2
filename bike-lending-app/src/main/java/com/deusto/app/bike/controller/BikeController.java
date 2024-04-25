cba705491c4ab8d897c03f4c6bfa2645d04e7c88
Aitorintx <93260675+Aitorintx@users.noreply.github.com>
2024-04-25 20:48:02 +0200

    Bike Details

--- bike-lending-app/src/main/java/com/deusto/app/bike/controller/BikeController.java
+++ bike-lending-app/src/main/java/com/deusto/app/bike/controller/BikeController.java
@@ -1,12 +1,13 @@
 package com.deusto.app.bike.controller;
 
+import java.util.ArrayList;
+
 import org.apache.logging.log4j.LogManager;
 
 
 import com.deusto.app.client.remote.ServiceLocator;
 
-
-
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.client.Entity;
 import jakarta.ws.rs.client.Invocation;
@@ -16,7 +17,7 @@ import jakarta.ws.rs.core.Response;
 
 public class BikeController {
 	private static BikeController instance;
-
+	
     private BikeController() {}
 
     public static synchronized BikeController getInstance() {
@@ -26,6 +27,24 @@ public class BikeController {
         return instance;
     }
     
+    
+    //Display Bike Info
+    public Response getBikeDetails(@PathParam("bikeId") int bikeId, @QueryParam("token") long token) {
+    	WebTarget displaySBWebTarget = ServiceLocator.getInstance().getWebTarget().path("bikeapp/bike/{bikeId}");
+        Invocation.Builder invocationBuilder = displaySBWebTarget.request(MediaType.APPLICATION_JSON);
+        
+        Response response = invocationBuilder.post(Entity.entity( MediaType.APPLICATION_JSON));
+        if (response.getStatus() == Response.Status.OK.getStatusCode()) {
+        	Response answer  = response.readEntity(Response.class);
+        	LogManager.getLogger(BikeController.class).info("Bike number {} displayed", bikeId);
+            return answer;
+            
+        } else {
+        	LogManager.getLogger(BikeController.class).error("Display failed. Code: {}, Reason: {}", response.getStatus(), response.readEntity(String.class));
+            return null;
+        }
+        
+    }
     //Display Stations & Bikes
     public Response displayStationsAndBikes(@QueryParam("token") long token) {
     	WebTarget displaySBWebTarget = ServiceLocator.getInstance().getWebTarget().path("bikeapp/bike/stations");
@@ -43,6 +62,8 @@ public class BikeController {
         }
         
     }
+    
+
     
     
 }
