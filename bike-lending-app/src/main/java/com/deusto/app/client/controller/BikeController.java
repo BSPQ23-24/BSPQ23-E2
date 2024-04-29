fe57b15d3e646ba512a25bd9cc89cfc13206e5fa
Aitorintx <93260675+Aitorintx@users.noreply.github.com>
2024-04-29 20:03:41 +0200

    Arreglados los tipos de controllers

--- bike-lending-app/src/main/java/com/deusto/app/client/controller/BikeController.java
+++ bike-lending-app/src/main/java/com/deusto/app/client/controller/BikeController.java
@@ -1,16 +1,20 @@
 package com.deusto.app.client.controller;
 
+import java.util.List;
+
 import org.apache.logging.log4j.LogManager;
 
 
 import com.deusto.app.client.remote.ServiceLocator;
 import com.deusto.app.server.pojo.BicycleData;
+import com.deusto.app.server.pojo.StationData;
 
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.client.Entity;
 import jakarta.ws.rs.client.Invocation;
 import jakarta.ws.rs.client.WebTarget;
+import jakarta.ws.rs.core.GenericType;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
@@ -28,7 +32,7 @@ public class BikeController {
     
     
     //Display Bike Info
-    public Response getBikeDetails(@PathParam("bikeId") int bikeId, @QueryParam("token") long token) {
+    public BicycleData getBikeDetails(@PathParam("bikeId") int bikeId, @QueryParam("token") long token) {
         WebTarget bikeDetailsWebTarget = ServiceLocator.getInstance().getWebTarget()
                                                      .path("bikeapp/bike/{bikeId}")
                                                      .resolveTemplate("bikeId", bikeId)
@@ -37,34 +41,36 @@ public class BikeController {
         Response response = invocationBuilder.get();
 
         if (response.getStatus() == Response.Status.OK.getStatusCode()) {
+        	BicycleData bikeSelected=response.readEntity(BicycleData.class);
             LogManager.getLogger(BikeController.class).info("Bike number {} displayed", bikeId);
-            return Response.ok(response.getEntity()).build();
+            return bikeSelected;
         } else {
             LogManager.getLogger(BikeController.class).error("Display failed. Code: {}, Reason: {}", response.getStatus(), response.readEntity(String.class));
-            return Response.ok(response.getEntity()).build();
+            return null;
         }
     }
 
-    //Display Stations & Bikes
-    public Response displayStationsAndBikes(@QueryParam("token") long token) {
+    //Display Stations 
+    public List<StationData> displayStations(@QueryParam("token") long token) {
     	WebTarget displaySBWebTarget = ServiceLocator.getInstance().getWebTarget().path("bikeapp/bike/stations")
     																			  .queryParam("token", token);
         Invocation.Builder invocationBuilder = displaySBWebTarget.request(MediaType.APPLICATION_JSON);
         
         Response response = invocationBuilder.get();
         if (response.getStatus() == Response.Status.OK.getStatusCode()) {
-        	LogManager.getLogger(BikeController.class).info("All the Stations and Bikes are displayed!");
-        	return Response.ok(response.getEntity()).build();
+        	List<StationData> listStations=response.readEntity(new GenericType<List<StationData>>() {});
+        	LogManager.getLogger(BikeController.class).info("All the Stations displayed!");
+        	return listStations;
             
         } else {
         	LogManager.getLogger(BikeController.class).error("Display failed. Code: {}, Reason: {}", response.getStatus(), response.readEntity(String.class));
-        	return Response.ok(response.getEntity()).build();
+        	return null;
         }
         
     }
     
     //Get all available bikes In Station
-    public Response getAvailableBikesInStation( @QueryParam("stationId") int stationId,  @QueryParam("token") long token) {
+    public List<BicycleData> getAvailableBikesInStation( @QueryParam("stationId") int stationId,  @QueryParam("token") long token) {
     	WebTarget getBikesWebTarget = ServiceLocator.getInstance().getWebTarget().path("bikeapp/bike/available")
     																			  .queryParam("stationId", stationId)
 				  																  .queryParam("token", token);
@@ -72,17 +78,18 @@ public class BikeController {
 		
 		Response response = invocationBuilder.get();
 		if (response.getStatus() == Response.Status.OK.getStatusCode()) {
-		LogManager.getLogger(BikeController.class).info("The bikes of the station {} are displayed!", stationId);
-		return Response.ok(response.getEntity()).build();
+			List<BicycleData> listBikes=response.readEntity(new GenericType<List<BicycleData>>() {});
+			LogManager.getLogger(BikeController.class).info("The bikes of the station {} are displayed!", stationId);
+			return listBikes;
 		
 		} else {
-		LogManager.getLogger(BikeController.class).error("Display failed. Code: {}, Reason: {}", response.getStatus(), response.readEntity(String.class));
-		return Response.ok(response.getEntity()).build();
+			LogManager.getLogger(BikeController.class).error("Display failed. Code: {}, Reason: {}", response.getStatus(), response.readEntity(String.class));
+			return null;
 		}
     }
     
 	//Bike Selection
-	public Response selectBike( @QueryParam("stationId") int stationId,  @QueryParam("token") long token) {
+	public BicycleData selectBike( @QueryParam("stationId") int stationId,  @QueryParam("token") long token) {
 	    WebTarget selectBikeWebTarget = ServiceLocator.getInstance().getWebTarget().path("bikeapp/bike/select")
 	    																		  .queryParam("stationId", stationId)
 					  															  .queryParam("token", token);
@@ -90,19 +97,20 @@ public class BikeController {
 			
 		Response response = invocationBuilder.get();
 		if (response.getStatus() == Response.Status.OK.getStatusCode()) {
+		BicycleData bikeSelected=response.readEntity(BicycleData.class);
 		LogManager.getLogger(BikeController.class).info("Bike selected!");
-		return Response.ok(response.getEntity()).build();
+		return bikeSelected;
 			
 		} else {
 		LogManager.getLogger(BikeController.class).error("Display failed. Code: {}, Reason: {}", response.getStatus(), response.readEntity(String.class));
-		return Response.ok(response.getEntity()).build();
+		return null;
 		}
 
 	}
 	
 	//Bike Creation
 	
-	public Response createBike( @QueryParam("stationId") int stationId, BicycleData bikeData, @QueryParam("token") long token) {
+	public boolean createBike( @QueryParam("stationId") int stationId, BicycleData bikeData, @QueryParam("token") long token) {
 	    WebTarget createBikeWebTarget = ServiceLocator.getInstance().getWebTarget().path("bikeapp/bike/create")
 	    																		  .queryParam("stationId", stationId)
 					  															  .queryParam("token", token);
@@ -112,12 +120,12 @@ public class BikeController {
 		Response response = invocationBuilder.post(Entity.entity(bikeData, MediaType.APPLICATION_JSON));
 		
 		if (response.getStatus() == Response.Status.OK.getStatusCode()) {
-		LogManager.getLogger(BikeController.class).info("Bike Created!");
-		return Response.ok(response.getEntity()).build();
+			LogManager.getLogger(BikeController.class).info("Bike Created!");
+			return true;
 			
 		} else {
-		LogManager.getLogger(BikeController.class).error("Display failed. Code: {}, Reason: {}", response.getStatus(), response.readEntity(String.class));
-		return Response.ok(response.getEntity()).build();
+			LogManager.getLogger(BikeController.class).error("Display failed. Code: {}, Reason: {}", response.getStatus(), response.readEntity(String.class));
+			return false;
 		}
 
 	}
