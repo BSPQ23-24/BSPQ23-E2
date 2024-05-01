2aa105718a9d338f32808c39c3164f5d66cd2430
alru28 <alvaroruiz@opendeusto.es>
2024-05-01 17:07:03 +0200

    Inicializacion BikeClient

--- bike-lending-app/src/main/java/com/deusto/app/client/main/BikeClient.java
+++ bike-lending-app/src/main/java/com/deusto/app/client/main/BikeClient.java
@@ -1,12 +1,19 @@
 package com.deusto.app.client.main;
 
+import com.deusto.app.client.UI.LoginUI;
+import com.deusto.app.client.controller.UserController;
 import com.deusto.app.client.remote.ServiceLocator;
+import com.deusto.app.server.pojo.UserData;
 
 public class BikeClient {
 
 	public static void main(String[] args) {
-		ServiceLocator.getInstance().setService(args[0], args[1]);
-
+		ServiceLocator.getInstance().setService("127.0.0.1", "8080");
+		
+		LoginUI loginUi = new LoginUI();
+		loginUi.showLogin();
+		
+		
 		// Example of user registration
 		/*
 		 * UserData userData = new UserData(); userData.setDni("12345678A");
