0dc011648f42a1927b3c0d0326226401986d10c0
Aroa Gonzalez González <80825553+AroaGonzalez@users.noreply.github.com>
2024-05-19 13:57:37 +0200

    test adminService acabado

--- bike-lending-app/src/test/java/com/deusto/app/unitary/services/AdminServiceTest.java
+++ bike-lending-app/src/test/java/com/deusto/app/unitary/services/AdminServiceTest.java
@@ -3,6 +3,7 @@ package com.deusto.app.unitary.services;
 import static org.mockito.Mockito.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+import javax.jdo.JDOObjectNotFoundException;
 import javax.jdo.PersistenceManager;
 import javax.jdo.Transaction;
 
@@ -30,7 +31,6 @@ public class AdminServiceTest {
     @InjectMocks
     private AdminService adminService;
 
-
     @Test
     public void testAddBike_Success() {
         BicycleData bikeData = new BicycleData();
@@ -40,12 +40,66 @@ public class AdminServiceTest {
 
         Station station = new Station();
         station.setId(1);
-        
+
         boolean result = adminService.addBike(bikeData);
 
+        assertTrue(result);
+    }
+
+    @Test
+    public void testDeleteBike_Success() {
+        Bicycle bike = mock(Bicycle.class);
+        when(pm.getObjectById(Bicycle.class, 1)).thenReturn(bike);
+        when(pm.currentTransaction()).thenReturn(tx); 
+        when(tx.isActive()).thenReturn(true);
+
+        boolean result = adminService.deleteBike(1);
+
+        verify(tx).begin();
+        verify(pm).deletePersistent(bike);
         verify(tx).commit();
         assertTrue(result);
     }
 
+    @Test
+    public void testDeleteBike_NotFound() {
+        doThrow(new JDOObjectNotFoundException()).when(pm).getObjectById(Bicycle.class, 1);
+        when(pm.currentTransaction()).thenReturn(tx);
+        when(tx.isActive()).thenReturn(true);
+
+        boolean result = adminService.deleteBike(1);
+
+        verify(tx).begin();
+        verify(pm, never()).deletePersistent(any(Bicycle.class));
+        verify(tx).commit();
+        assertFalse(result);
+    }
     
+    @Test
+    public void testDisableBike_Success() {
+    	Bicycle bike = mock(Bicycle.class);
+        when(pm.getObjectById(Bicycle.class, 1)).thenReturn(bike);
+        
+        boolean result = adminService.disableBike(1);
+
+        verify(bike).setAvailable(false);
+        verify(pm).makePersistent(bike);
+        verify(tx).commit();
+        assertTrue(result);
+    }
+    
+    @Test
+    public void testAbleBike_Success() {
+    	Bicycle bike = mock(Bicycle.class);
+        when(pm.getObjectById(Bicycle.class, 1)).thenReturn(bike);
+        
+        boolean result = adminService.ableBike(1);
+
+        verify(bike).setAvailable(true);
+        verify(pm).makePersistent(bike);
+        verify(tx).commit();
+        assertTrue(result);
+    }
+
+
 }
