9ffec251900493c3049a31953755129a8c2ccb71
alru28 <alvaroruiz@opendeusto.es>
2024-05-20 18:03:17 +0200

    Fix services
    Fresh PM and TX. And detach copy user.

--- bike-lending-app/src/main/java/com/deusto/app/server/services/AdminService.java
+++ bike-lending-app/src/main/java/com/deusto/app/server/services/AdminService.java
@@ -3,216 +3,200 @@ package com.deusto.app.server.services;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.annotation.Nonnull;
 import javax.jdo.JDOHelper;
 import javax.jdo.JDOObjectNotFoundException;
 import javax.jdo.PersistenceManager;
 import javax.jdo.PersistenceManagerFactory;
 import javax.jdo.Transaction;
-import javax.jdo.annotations.Persistent;
 
 import org.apache.logging.log4j.LogManager;
 
 import com.deusto.app.server.data.domain.Bicycle;
 import com.deusto.app.server.data.domain.Station;
-import com.deusto.app.server.data.domain.User;
 import com.deusto.app.server.pojo.BicycleData;
-import com.deusto.app.server.pojo.StationAssembler;
-import com.deusto.app.server.pojo.StationData;
 
 public class AdminService {
 	private static AdminService instance;
 
 	private PersistenceManagerFactory pmf;
-	private PersistenceManager pm;
-	private Transaction tx;
 
 	/**
-     * Private constructor to initialize PersistenceManager and Transaction.
-     */
+	 * Private constructor to initialize PersistenceManagerFactory.
+	 */
 	private AdminService() {
 		pmf = JDOHelper.getPersistenceManagerFactory("datanucleus.properties");
-		pm = pmf.getPersistenceManager();
-		tx = pm.currentTransaction();
 	}
 
 	/**
-     * Returns the singleton instance of BikeService.
-     *
-     * @return BikeService instance
-     */
+	 * Returns the singleton instance of AdminService.
+	 *
+	 * @return AdminService instance
+	 */
 	public static AdminService getInstance() {
 		if (instance == null) {
 			instance = new AdminService();
 		}
 		return instance;
 	}
-	
-	
+
 	/**
-     * Adds a bike to a station.
-     *
-     * @return Add a bicycle 
-     */
-	
-	 public boolean addBike(BicycleData bikeData) { 
+	 * Adds a bike to a station.
+	 *
+	 * @return true if the bike is added successfully, false otherwise
+	 */
+	public boolean addBike(BicycleData bikeData) {
+		PersistenceManager pm = pmf.getPersistenceManager();
+		Transaction tx = pm.currentTransaction();
+		try {
+			tx.begin();
+			Bicycle bike = new Bicycle();
+			LogManager.getLogger(AdminService.class).info("Add | Bike: '{}'", bike.getId());
+
 			try {
-				tx.begin();
-				Bicycle bike = new Bicycle();
-				LogManager.getLogger(AdminService.class).info("Add | Bike: '{}'", bike.getId());
-				try {
-					bike = pm.getObjectById(Bicycle.class, bike.getId());
-					// If the user is found, return an unauthorized response
-					if (bike != null) {
-						LogManager.getLogger(AdminService.class)
-								.info("Addition Failed | The bike is already added | Bike: '{}'", bike.getId());
-						return false;
-					}
-				} catch (javax.jdo.JDOObjectNotFoundException jonfe) {
-					// User not found, proceed to registration
-					
-					
-					bike.setAcquisitionDate(bikeData.getAcquisitionDate());
-					bike.setType(bikeData.getType());
-					bike.setAvailable(true);
-					
-					Station station= modifyStation(bikeData.getStationId(), bike);
-					bike.setStation(station);
-					pm.makePersistent(bike);
-					System.out.println(bike.getStation().getBikes());
-					LogManager.getLogger(AdminService.class).info("Addition Success | Bike: '{}'", bike.getId());
-					
+				bike = pm.getObjectById(Bicycle.class, bike.getId());
+				// If the bike is found, return a failure response
+				if (bike != null) {
+					LogManager.getLogger(AdminService.class)
+							.info("Addition Failed | The bike is already added | Bike: '{}'", bike.getId());
+					return false;
 				}
-				
-				tx.commit();
-				return true;
-			} finally {
-				if (tx.isActive()) {
-					tx.rollback();
-				}
-			}
-	 }
-	 
-	 public Station modifyStation(int idStation, Bicycle bike) { 
-		 	LogManager.getLogger(AdminService.class).info("Modify | Station: '{}'", idStation);
-			try {
-				Station station = null;
-				
+			} catch (javax.jdo.JDOObjectNotFoundException jonfe) {
+				// Bike not found, proceed to addition
+
+				bike.setAcquisitionDate(bikeData.getAcquisitionDate());
+				bike.setType(bikeData.getType());
+				bike.setAvailable(true);
+
+				// Modify station logic moved here
+				int idStation = bikeData.getStationId();
+				LogManager.getLogger(AdminService.class).info("Modify | Station: '{}'", idStation);
+				Station station;
 				try {
 					station = pm.getObjectById(Station.class, idStation);
-					// If the user is found, return an unauthorized response
+					// If the station is not found, return a failure response
 					if (station == null) {
 						LogManager.getLogger(AdminService.class)
 								.info("Addition Failed | The station does not exist | Station: '{}'", idStation);
-						return null;
+						return false;
 					}
-				} catch (javax.jdo.JDOObjectNotFoundException jonfe) {}
-					// User not found, proceed to registration
-					
-					
-					
-					List<Bicycle> bikes=new ArrayList<Bicycle>();
-					for (Bicycle b: station.getBikes()) {
-						bikes.add(b);
-					}
-					bikes.add(bike);
-					station.setBikes(bikes);
-					
-					
-					
-					return station;
-			} finally {
-				
+				} catch (javax.jdo.JDOObjectNotFoundException jonfe2) {
+					LogManager.getLogger(AdminService.class)
+							.info("Addition Failed | The station does not exist | Station: '{}'", idStation);
+					return false;
+				}
+
+				List<Bicycle> bikes = new ArrayList<>(station.getBikes());
+				bikes.add(bike);
+				station.setBikes(bikes);
+				bike.setStation(station);
+
+				pm.makePersistent(bike);
+				pm.makePersistent(station); // Persist the modified station
+				System.out.println(bike.getStation().getBikes());
+				LogManager.getLogger(AdminService.class).info("Addition Success | Bike: '{}'", bike.getId());
 			}
-	 }
-	 
-	 public boolean deleteBike(int bikeId) {
+
+			tx.commit();
+			return true;
+		} catch (Exception e) {
+			LogManager.getLogger(AdminService.class).error("Addition Failed | Exception: {}", e.getMessage());
+			return false;
+		} finally {
+			if (tx.isActive()) {
+				tx.rollback();
+			}
+			pm.close();
+		}
+	}
+
+	public boolean deleteBike(int bikeId) {
+		PersistenceManager pm = pmf.getPersistenceManager();
+		Transaction tx = pm.currentTransaction();
 		LogManager.getLogger(AdminService.class).info("Delete | Bike: '{}'", bikeId);
-        Transaction tx = pm.currentTransaction();
-        
-        try {
-            tx.begin();
-            try {
-                Bicycle bike = pm.getObjectById(Bicycle.class, bikeId);
-                if (bike != null) {
-                    pm.deletePersistent(bike);
-                    LogManager.getLogger(AdminService.class).info("Deletion Success | Bike: '{}'", bikeId);
-                    tx.commit();
-                    return true;
-                }
-            } catch (JDOObjectNotFoundException jonfe) {
-                LogManager.getLogger(AdminService.class).info("This bike does not exist | Bike: '{}'", bikeId);
-            }
-            tx.commit();
-        } catch (Exception e) {
-            if (tx.isActive()) {
-                tx.rollback();
-            }
-            throw e;
-        } finally {
-            if (tx.isActive()) {
-                tx.rollback();
-            }
-        }
-        return false;
-	    
-	 }
-	 
-	 public boolean disableBike(int bikeId) {
-		 LogManager.getLogger(AdminService.class).info("Disable | Bike: '{}'", bikeId);
-		 try {
-			 tx.begin();
-			 Bicycle bike=null;
-			 try {
-				 bike=pm.getObjectById(Bicycle.class,bikeId);
-				 if (bike==null) {
-					 LogManager.getLogger(AdminService.class)
-						.info("This bike do not exist | Bike: '{}'", bikeId);
-					 return false;
-				 }
-				 bike.setAvailable(false);
-		         pm.makePersistent(bike);
-			  } catch (javax.jdo.JDOObjectNotFoundException jonfe) {
-				  LogManager.getLogger(AdminService.class).info("Bike not found, cannot disable | Bike: '{}'", bikeId);
-		            return false;
-		        }
-		        tx.commit();
-		        return true;
-		    } finally {
-		        if (tx.isActive()) {
-		            tx.rollback();
-		        }
-		    }
-	 }
-	 
-	 public boolean ableBike(int bikeId) {
-		 LogManager.getLogger(AdminService.class).info("Able | Bike: '{}'", bikeId);
-		 try {
-			 tx.begin();
-			 Bicycle bike=null;
-			 try {
-				 bike=pm.getObjectById(Bicycle.class,bikeId);
-				 if (bike==null) {
-					 LogManager.getLogger(AdminService.class)
-						.info("This bike do not exist | Bike: '{}'", bikeId);
-					 return false;
-				 }
-				 bike.setAvailable(true);
-				 pm.makePersistent(bike);
-			  } catch (javax.jdo.JDOObjectNotFoundException jonfe) {
-				  LogManager.getLogger(AdminService.class).info("Bike not found, cannot disable | Bike: '{}'", bikeId);
-		          return false;
-			 }
-			 
-			 tx.commit();
-			 return true;
-		 
-		 } finally {
-			 if(tx.isActive()) {
-				 tx.rollback();
-			 }
-		 }
-	 }
-	 
-	 
+
+		try {
+			tx.begin();
+			try {
+				Bicycle bike = pm.getObjectById(Bicycle.class, bikeId);
+				if (bike != null) {
+					pm.deletePersistent(bike);
+					LogManager.getLogger(AdminService.class).info("Deletion Success | Bike: '{}'", bikeId);
+					tx.commit();
+					return true;
+				}
+			} catch (JDOObjectNotFoundException jonfe) {
+				LogManager.getLogger(AdminService.class).info("This bike does not exist | Bike: '{}'", bikeId);
+			}
+			tx.commit();
+		} catch (Exception e) {
+			if (tx.isActive()) {
+				tx.rollback();
+			}
+			throw e;
+		} finally {
+			if (tx.isActive()) {
+				tx.rollback();
+			}
+			pm.close();
+		}
+		return false;
+	}
+
+	public boolean disableBike(int bikeId) {
+		PersistenceManager pm = pmf.getPersistenceManager();
+		Transaction tx = pm.currentTransaction();
+		LogManager.getLogger(AdminService.class).info("Disable | Bike: '{}'", bikeId);
+		try {
+			tx.begin();
+			Bicycle bike = null;
+			try {
+				bike = pm.getObjectById(Bicycle.class, bikeId);
+				if (bike == null) {
+					LogManager.getLogger(AdminService.class).info("This bike do not exist | Bike: '{}'", bikeId);
+					return false;
+				}
+				bike.setAvailable(false);
+				pm.makePersistent(bike);
+			} catch (javax.jdo.JDOObjectNotFoundException jonfe) {
+				LogManager.getLogger(AdminService.class).info("Bike not found, cannot disable | Bike: '{}'", bikeId);
+				return false;
+			}
+			tx.commit();
+			return true;
+		} finally {
+			if (tx.isActive()) {
+				tx.rollback();
+			}
+			pm.close();
+		}
+	}
+
+	public boolean ableBike(int bikeId) {
+		PersistenceManager pm = pmf.getPersistenceManager();
+		Transaction tx = pm.currentTransaction();
+		LogManager.getLogger(AdminService.class).info("Able | Bike: '{}'", bikeId);
+		try {
+			tx.begin();
+			Bicycle bike = null;
+			try {
+				bike = pm.getObjectById(Bicycle.class, bikeId);
+				if (bike == null) {
+					LogManager.getLogger(AdminService.class).info("This bike do not exist | Bike: '{}'", bikeId);
+					return false;
+				}
+				bike.setAvailable(true);
+				pm.makePersistent(bike);
+			} catch (javax.jdo.JDOObjectNotFoundException jonfe) {
+				LogManager.getLogger(AdminService.class).info("Bike not found, cannot disable | Bike: '{}'", bikeId);
+				return false;
+			}
+			tx.commit();
+			return true;
+		} finally {
+			if (tx.isActive()) {
+				tx.rollback();
+			}
+			pm.close();
+		}
+	}
 }
--- bike-lending-app/src/main/java/com/deusto/app/server/services/LoanService.java
+++ bike-lending-app/src/main/java/com/deusto/app/server/services/LoanService.java
@@ -13,97 +13,97 @@ import com.deusto.app.server.pojo.LoanData;
 
 public class LoanService {
 
-    private static LoanService instance;
-    private PersistenceManagerFactory pmf;
+	private static LoanService instance;
+	private PersistenceManagerFactory pmf;
 
-    private LoanService() {
-        pmf = JDOHelper.getPersistenceManagerFactory("datanucleus.properties");
-    }
+	private LoanService() {
+		pmf = JDOHelper.getPersistenceManagerFactory("datanucleus.properties");
+	}
 
-    public static LoanService getInstance() {
-        if (instance == null) {
-            instance = new LoanService();
-        }
-        return instance;
-    }
+	public static LoanService getInstance() {
+		if (instance == null) {
+			instance = new LoanService();
+		}
+		return instance;
+	}
 
-    public List<LoanData> getAllLoans() {
-        PersistenceManager pm = pmf.getPersistenceManager();
-        Transaction tx = pm.currentTransaction();
-        List<LoanData> loanDataList = new ArrayList<>();
-        try {
-            tx.begin();
-            Query<Loan> query = pm.newQuery(Loan.class);
-            List<Loan> loans = (List<Loan>) query.execute();
-            for (Loan loan : loans) {
-                LoanData loanData = new LoanData();
-                loanData.setId(loan.getId());
-                loanData.setLoanDate(loan.getLoanDate());
-                loanData.setStartHour(loan.getStartHour());
-                loanData.setEndHour(loan.getEndHour());
-                loanData.setUserDni(loan.getId());
-                loanData.setBicycleId(loan.getBicycle().getId());
-                loanDataList.add(loanData);
-            }
-            tx.commit();
-        } catch (Exception e) {
-            if (tx.isActive()) {
-                tx.rollback();
-            }
-            e.printStackTrace();
-        } finally {
-            pm.close();
-        }
-        return loanDataList;
-    }
+	public List<LoanData> getAllLoans() {
+		PersistenceManager pm = pmf.getPersistenceManager();
+		Transaction tx = pm.currentTransaction();
+		List<LoanData> loanDataList = new ArrayList<>();
+		try {
+			tx.begin();
+			Query<Loan> query = pm.newQuery(Loan.class);
+			List<Loan> loans = (List<Loan>) query.execute();
+			for (Loan loan : loans) {
+				LoanData loanData = new LoanData();
+				loanData.setId(loan.getId());
+				loanData.setLoanDate(loan.getLoanDate());
+				loanData.setStartHour(loan.getStartHour());
+				loanData.setEndHour(loan.getEndHour());
+				loanData.setUserDni(loan.getId());
+				loanData.setBicycleId(loan.getBicycle().getId());
+				loanDataList.add(loanData);
+			}
+			tx.commit();
+		} catch (Exception e) {
+			if (tx.isActive()) {
+				tx.rollback();
+			}
+			e.printStackTrace();
+		} finally {
+			pm.close();
+		}
+		return loanDataList;
+	}
 
-    public boolean createLoan(LoanData loanData) {
-        PersistenceManager pm = pmf.getPersistenceManager();
-        Transaction tx = pm.currentTransaction();
-        try {
-            tx.begin();
-            Loan loan = new Loan();
-            loan.setId(loanData.getId()); 
-            loan.setLoanDate(loanData.getLoanDate());
-            loan.setStartHour(loanData.getStartHour());
-            loan.setEndHour(loanData.getEndHour());
-            loan.setUser(loan.getUser());
-            loan.setBicycle(loan.getBicycle());
-            pm.makePersistent(loan);
-            tx.commit();
-            return true;
-        } catch (Exception e) {
-            if (tx.isActive()) {
-                tx.rollback();
-            }
-            e.printStackTrace();
-            return false;
-        } finally {
-            pm.close();
-        }
-    }
+	public boolean createLoan(LoanData loanData) {
+		PersistenceManager pm = pmf.getPersistenceManager();
+		Transaction tx = pm.currentTransaction();
+		try {
+			tx.begin();
+			Loan loan = new Loan();
+			loan.setId(loanData.getId());
+			loan.setLoanDate(loanData.getLoanDate());
+			loan.setStartHour(loanData.getStartHour());
+			loan.setEndHour(loanData.getEndHour());
+			loan.setUser(loan.getUser());
+			loan.setBicycle(loan.getBicycle());
+			pm.makePersistent(loan);
+			tx.commit();
+			return true;
+		} catch (Exception e) {
+			if (tx.isActive()) {
+				tx.rollback();
+			}
+			e.printStackTrace();
+			return false;
+		} finally {
+			pm.close();
+		}
+	}
 
-    public boolean deleteLoan(int loanId) {
-        PersistenceManager pm = pmf.getPersistenceManager();
-        Transaction tx = pm.currentTransaction();
-        try {
-            tx.begin();
-            Loan loan = pm.getObjectById(Loan.class, loanId);
-            if (loan != null) {
-                pm.deletePersistent(loan);
-                tx.commit();
-                return true;
-            } else {
-                return false;
-            }
-        } catch (Exception e) {
-            if (tx.isActive()) {
-                tx.rollback();
-            }
-            e.printStackTrace();
-            return false;
-        } finally {
-            pm.close();
-        }
-    }
+	public boolean deleteLoan(int loanId) {
+		PersistenceManager pm = pmf.getPersistenceManager();
+		Transaction tx = pm.currentTransaction();
+		try {
+			tx.begin();
+			Loan loan = pm.getObjectById(Loan.class, loanId);
+			if (loan != null) {
+				pm.deletePersistent(loan);
+				tx.commit();
+				return true;
+			} else {
+				return false;
+			}
+		} catch (Exception e) {
+			if (tx.isActive()) {
+				tx.rollback();
+			}
+			e.printStackTrace();
+			return false;
+		} finally {
+			pm.close();
+		}
+	}
 }
