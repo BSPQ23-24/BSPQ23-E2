4f51041289bee4640b714b1c53e80777c378ef67
jorgealcorta <jorgealcorta@opendeusto.es>
2024-05-21 09:00:09 +0200

    WORKS but local client does not connect

--- bike-lending-app/sql/create-bikes.sql
+++ bike-lending-app/sql/create-bikes.sql
@@ -1,11 +1,11 @@
 /* DELETE 'bikesDB' database*/
 DROP SCHEMA IF EXISTS bikesdb;
 /* DELETE USER 'spq' AT LOCAL SERVER*/
-DROP USER IF EXISTS 'spq'@'localhost';
+DROP USER IF EXISTS 'spq'@'mysql';
 
 /* CREATE 'messagesDB' DATABASE */
 CREATE SCHEMA bikesdb;
 /* CREATE THE USER 'spq' AT LOCAL SERVER WITH PASSWORD 'spq' */
-CREATE USER IF NOT EXISTS 'spq'@'localhost' IDENTIFIED BY 'spq';
+CREATE USER IF NOT EXISTS 'spq'@'mysql' IDENTIFIED BY 'spq';
 
-GRANT ALL ON bikesdb.* TO 'spq'@'localhost';
+GRANT ALL ON bikesdb.* TO 'spq'@'mysql';
